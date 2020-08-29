bd85beafb5fbdf9eab2ce4f63648d8d6c54a65f6
Matthew Cowley <matthewcowley@Matthews-MacBook-Air.local>
2020-08-29 18:53:28 -0400

    made files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # timed-quiz
+## Description
+---
+This is a timed quiz, that is made through the DOM. Providing questions with multiple choice questions.
+
+[Timed-Quiz](https://mcowley1.github.io/timed-quiz/)
+
+![]()
---
+++ dom.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+      quiz: "What's your mom's favorite fruit?\n(a) banana\n(b) apple\n(c) acai",
+      answer: "a"  
+    },
+    {
+        quiz: "What's your favorite color?\n(a) pink\n(b) pink\n(c) pink",
+        answer: "b"
+    },
+    {
+        quiz: "What is your mom's best snack ever?\n(a) cookies\n(b) pizza\n(c) cake",
+        answer: "a"
+    }
+];
+
+var score = 0;
+
